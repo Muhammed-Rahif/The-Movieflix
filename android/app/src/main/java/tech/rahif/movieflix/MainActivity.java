b5dc2587fd9ab4c90355828895b6d65abbd5255e
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-07-19 19:54:26 +0530

    project start

---
+++ android/app/src/main/java/tech/rahif/movieflix/MainActivity.java
@@ -0,0 +1,5 @@
+package tech.rahif.movieflix;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tech.rahif.movieflix',
+  appName: 'movieflix',
+  webDir: 'dist'
+};
+
+export default config;
