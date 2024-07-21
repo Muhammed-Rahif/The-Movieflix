55c2254548e1692a03810c2a3d0dfcdc5918df3e
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-07-22 01:11:43 +0530

    feat(icons): update app name and icons; back btn closes app issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'tech.rahif.movieflix',
-  appName: 'movieflix',
-  webDir: 'dist'
+  appId: "tech.rahif.movieflix",
+  appName: "Movieflix",
+  webDir: "dist",
 };
 
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
+import { useEffect } from "react";
+import { App as CapacitorApp } from "@capacitor/app";
 
 import "./App.css";
 
 function App() {
   const isLoggedIn = false;
+
+  useEffect(() => {
+    // https://stackoverflow.com/a/69084017/14781260
+    CapacitorApp.addListener("backButton", ({ canGoBack }) => {
+      if (!canGoBack) {
+        CapacitorApp.exitApp();
+      } else {
+        window.history.back();
+      }
+    });
+  }, []);
 
   return (
     <div
