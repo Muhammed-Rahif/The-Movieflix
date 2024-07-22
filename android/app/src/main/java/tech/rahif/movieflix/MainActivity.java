31bf2e1952510fd890e1d83e99347adef5491e94
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-07-22 23:34:13 +0530

    fix(mobile): scroll bar not hidden

--- android/app/src/main/java/tech/rahif/movieflix/MainActivity.java
+++ android/app/src/main/java/tech/rahif/movieflix/MainActivity.java
@@ -2,4 +2,11 @@ package tech.rahif.movieflix;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  // https://github.com/ionic-team/capacitor/issues/5469#issuecomment-1129924289
+  @Override
+  public void onStart() {
+    super.onStart();
+    bridge.getWebView().setVerticalScrollBarEnabled(false);
+  }
+}
