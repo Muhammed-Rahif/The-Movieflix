4a810f43d90ae212c9d9ec20090a69cb8c7c36da
Muhammed-Rahif <rahifpalliyalil@gmail.com>
2024-07-27 14:50:49 +0530

    fix(material-tailwind): version types issue
    https://github.com/creativetimofficial/material-tailwind/issues/528
    https://github.com/creativetimofficial/material-tailwind/issues/427

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { ButtonStyleTypes, ThemeProvider } from "@material-tailwind/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -22,7 +22,7 @@ createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider value={theme}>{(<App />) as ReactElement}</ThemeProvider>
+        <ThemeProvider value={theme}>{(<App />) as any}</ThemeProvider>
 
         {/* <ReactQueryDevtools buttonPosition="top-right" /> */}
       </QueryClientProvider>
